612cab17e7a50a44822c641e977178fd9402153b
Desyibelew <desuk2012@gmail.com>
2022-04-22 11:51:21 +0300

    Second edited Ecommerce code commit

---
+++ resources/views/product/register.blade.php
@@ -0,0 +1,36 @@
+<html>
+    <header>
+        <title>ecommerce</title>
+    </header>
+    <body style="background-color:#272B4F;">
+    <center>
+        <font color ="#D011E7">
+        <h1>REGISTRATION PAGE</h1>
+</font>
+     <form>
+         <table>
+             <tr>
+                 <td  bgcolor="green">Name:</td>
+                    <td><input type="name" name="name" id="name" placeholder="Enter name"></td>
+</tr>
+<tr>
+    <td  bgcolor="green">Unit:</td>
+       <td><input type="Unit" name="Unit" id="unit" placeholder="Enter unit"> </td>
+</tr>
+<tr>
+    <td  bgcolor="green">Price:</td>
+       <td> <input type="Price"name="Price" id="price" placeholder="Enter price"> </td>
+</tr>
+<tr>
+    <td  bgcolor="green">Quantity:</td>
+        <td><input type="Quantity"name="Quantity" id="quantity" placeholder="Enter quantity"> </td>
+</tr>
+</table>
+</form>
+</center>
+<center>
+<button style="background-color: blue;color: white;border: 1px solid #e4e4e4; padding: 8px;border-radius: 3px;cursor: pointer;">Register</button>
+<br>
+</center>
+</body>
+</html>
